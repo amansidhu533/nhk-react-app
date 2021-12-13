164ed92cecead04a8bda4b747697d96c409e871f
Aman Sidhu <amansidhu@LAPTOP-QVJIM6MH>
2021-12-13 16:44:43 +0530

    webpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
+  context: "_dirname",
+  entry: "./src/index;js",
+  output: {
+    path: path.resolve(__dirname, ""),
+    __filename: "main.js",
+    publicPath: "/",
+  },
   //...
   devServer: {
     historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
 };
